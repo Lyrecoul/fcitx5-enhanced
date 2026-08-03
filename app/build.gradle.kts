8bd0df958fd42aff0324110afa233a5508a85077
rebron1900 <me@1900.live>
2026-08-03 15:57:47 +0800

    feat: v1.9.1 — 用gpt优化了性能，修复了很多bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.rebron1900.fcitx5enhanced"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "1.9.0"
+        versionCode = 23
+        versionName = "1.9.1"
     }
 
     buildTypes {
